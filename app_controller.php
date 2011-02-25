55bb90d4c0edda5068fa8cf5a59f01307af44a74
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2011-02-25 14:52:59 -0430

    Improving app_controller

--- app_controller.php
+++ app_controller.php
@@ -27,8 +27,138 @@
  * Add your application-wide methods in the class below, your controllers
  * will inherit them.
  *
- * @package       cake
- * @subpackage    cake.app
  */
 class AppController extends Controller {
+
+/**
+ * Components
+ *
+ * @var array
+ * @access public
+ */
+	public $components = array('Auth', 'RequestHandler', 'Session', 'Cookie');
+
+/**
+ * Helpers
+ *
+ * @var array
+ * @access public
+ */
+	public $helpers = array('Session', 'Html');
+
+/**
+ * Object constructor - Adds the Debugkit panel if in development mode
+ *
+ */
+	public function __construct() {
+		if (Configure::read('debug')) {
+			$this->components[] = 'DebugKit.Toolbar';
+		}
+		parent::__construct();
+	}
+
+/**
+ * Before filter callback
+ *
+ * @return void
+ * @access public
+ */
+	public function beforeFilter() {
+		$this->_setupAuth();
+		$this->_beforeFilterAuth();
+	}
+
+/**
+ * Setup Authentication
+ *
+ * @return void
+ * @access protected
+ */
+	protected function _setupAuth() {
+		$this->Auth->authorize = 'controller';
+		$this->Auth->fields = array('username' => 'email', 'password' => 'passwd');
+		$this->Auth->loginAction = array('plugin' => 'users', 'admin' => false, 'controller' => 'users', 'action' => 'login');
+		$this->Auth->loginRedirect = '/';
+		$this->Auth->logoutRedirect = '/';
+		$this->Auth->authError = __('Sorry, but you need to be logged in to access this location.', true);
+		$this->Auth->loginError = __('Invalid credentials. Please try again or create an account.', true);
+		$this->Auth->autoRedirect = false;
+		$this->Auth->userModel = 'User';
+		$this->Auth->authenticate = null;
+		$this->Auth->sessionKey = 'Auth.User';
+		$this->Auth->userScope = array(
+			'User.email_authenticated' => 1,
+			'User.active' => 1
+		);
+	}
+
+/**
+ * beforeFilterAuth
+ *
+ * @access public
+ * @return void
+ */
+	protected function _beforeFilterAuth() {
+		$this->Cookie->domain = env('HTTP_BASE');
+		$this->Cookie->name = 'rememberMe';
+		$cookie = $this->Cookie->read('User');
+
+		if (!empty($cookie) && !$this->Auth->user()) {
+			$data['User'][$this->Auth->fields['username']] = $cookie[$this->Auth->fields['username']];
+			$data['User'][$this->Auth->fields['password']] = $cookie[$this->Auth->fields['password']];
+			$this->Auth->login($data);
+		}
+	}
+
+/**
+ * isAuthorized Auth callback
+ *
+ * @return boolean Whether the user is authorized to access the current page or not
+ * @access public
+ */
+	public function isAuthorized() {
+		$authorized = true;
+		return $authorized;
+	}
+
+/**
+ * Before render callback
+ *
+ * @return void
+ * @access public
+ */
+	public function beforeRender() {
+		$userData = $this->Auth->user();
+		$this->set('userData', $userData);
+	}
+
+/**
+ * Returns true if the action was called with requestAction()
+ *
+ * @return boolean
+ * @access public
+ */
+	public function isRequestedAction() {
+		return array_key_exists('requested', $this->params);
+	}
+
+
+/**
+ * Renders the json view if the request is preferred as json. Otherwise operats as a normal redirect
+ *
+ * @param mixed $url A string or array-based URL pointing to another location within the app,
+ *     or an absolute URL
+ * @param integer $status Optional HTTP status code (eg: 404)
+ * @param boolean $exit If true, exit() will be called after the redirect
+ * @return mixed void if $exit = false. Terminates script if $exit = true
+ * @access public
+ */
+	function redirect($url, $status = null, $exit = true) {
+		if (!empty($this->params['url']['ext']) && $this->params['url']['ext'] == 'json') {
+			$this->set('redirectUrl', $url);
+			echo $this->render();
+			$exit && $this->_stop();
+		}
+		parent::redirect($url, $status, $exit);
+	}
 }
