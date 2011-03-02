0c3bbc6bd3664de103c3ebe4b0372112cd78cf1d
Alejandro Ibarra <ajibarra@camila.(none)>
2011-03-01 21:37:35 -0430

    schema.php
    * addresses
    * contacts
    * bank_accounts
    * organizations

---
+++ config/migrations/initial_migration.php
@@ -0,0 +1,47 @@
+<?php
+class M4d6da4b70f3041b1be8718ac94e05dd2 extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+		),
+		'down' => array(
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+}
+?>
---
+++ config/migrations/map.php
@@ -0,0 +1,6 @@
+<?php
+$map = array(
+	1 => array(
+		'initial_migration' => 'M4d6da4b70f3041b1be8718ac94e05dd2'),
+);
+?>
